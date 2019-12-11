a2bc01bbdcd0cf3101f5e4c1e2d7220354b4dca5
long_dd <18608098621@163.com>
2019-12-11 23:06:00 +0800

    long_dd

---
+++ remove_repeat_vectors.py
@@ -0,0 +1,28 @@
+﻿class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        s = set(nums)
+        l = list(s)
+        l.sort()
+        n = len(l)
+        nums[:n]=l
+        return n
+#####java#####
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // 使用双指针
+        if(nums==null || nums.length == 1){
+            return nums.length;
+        }
+        int i = 0,j =1;
+        while(j<nums.length){
+            if(nums[i]==nums[j]){
+                j++;
+            }else{
+                i++;
+                nums[i]=nums[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
+}
