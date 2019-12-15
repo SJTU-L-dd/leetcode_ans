4dba3fde446bd9a3d55e02d53cc865a707396b90
longyuhao <18608098621@163.com>
2019-12-15 22:13:23 +0800

    longyuhao

---
+++ merge_array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2)
+
+#####java####
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    System.arraycopy(nums2, 0, nums1, m, n);
+    Arrays.sort(nums1);
+  }
+}
+
